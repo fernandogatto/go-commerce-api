a16a274ec4836b0b17a8c9947409eccbeb1098e4
fernandogatto <fernandogatto17@gmail.com>
2020-07-01 02:48:47 -0300

    Add products and orders repository

--- src/modules/orders/infra/typeorm/repositories/OrdersRepository.ts
+++ src/modules/orders/infra/typeorm/repositories/OrdersRepository.ts
@@ -12,11 +12,22 @@ class OrdersRepository implements IOrdersRepository {
   }
 
   public async create({ customer, products }: ICreateOrderDTO): Promise<Order> {
-    // TODO
+    const order = this.ormRepository.create({
+      customer,
+      order_products: products,
+    });
+
+    await this.ormRepository.save(order);
+
+    return order;
   }
 
   public async findById(id: string): Promise<Order | undefined> {
-    // TODO
+    const order = this.ormRepository.findOne({
+      where: { id },
+    });
+
+    return order;
   }
 }
 
--- src/modules/products/infra/typeorm/repositories/ProductsRepository.ts
+++ src/modules/products/infra/typeorm/repositories/ProductsRepository.ts
@@ -21,21 +21,50 @@ class ProductsRepository implements IProductsRepository {
     price,
     quantity,
   }: ICreateProductDTO): Promise<Product> {
-    // TODO
+    const product = this.ormRepository.create({
+      name,
+      price,
+      quantity,
+    });
+
+    await this.ormRepository.save(product);
+
+    return product;
   }
 
   public async findByName(name: string): Promise<Product | undefined> {
-    // TODO
+    const product = await this.ormRepository.findOne({
+      where: { name },
+    });
+
+    return product;
   }
 
   public async findAllById(products: IFindProducts[]): Promise<Product[]> {
-    // TODO
+    const allProducts = await this.ormRepository.find({
+      where: { id: In(products) },
+    });
+
+    return allProducts;
   }
 
   public async updateQuantity(
     products: IUpdateProductsQuantityDTO[],
   ): Promise<Product[]> {
-    // TODO
+    const products_id = products.map(product => product.id);
+
+    const storedProducts = await this.ormRepository.findByIds(products_id);
+
+    const updateProducts = storedProducts.map(storedProduct => {
+      const product = products.find(p => p.id === storedProduct.id);
+
+      return {
+        ...product,
+        quantity: product?.quantity,
+      };
+    });
+
+    return this.ormRepository.save(updateProducts);
   }
 }
 
